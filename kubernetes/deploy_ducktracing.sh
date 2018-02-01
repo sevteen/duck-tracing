ecdc0d23b329bdac96895adab92afb63c3e6f3fa
Beka Tsotsoria <b.tsotsoria@gmail.com>
2018-02-01 22:12:38 +0100

    Some fixes tu kubernetes resources

--- kubernetes/deploy_ducktracing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create -f ducktracing.yml
-
-echo "Auth URL: `minikube service auth --url`"
-echo "Ducky URL: `minikube service ducky --url`"
-echo "Jaeger URL: `minikube service jaeger-query --url`"
